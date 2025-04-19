c98895b608f32393c226e8e3ecdc08e3846bc56e
gyoungsubLee <dltjq2323@gmail.com>
2025-04-19 16:07:46 +0900

    Feat: [visual 영역] 인터렉티브 애니메이션 효과 적용

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 window.onload = function() {
     // 01 gnb 애니메이션
-
     const menuOpen = document.querySelector('.gnb .menuOpen');
     const menuBox = document.querySelector('.gnb .menuBox');
 
     menuOpen.addEventListener('click', function() {
         menuBox.classList.toggle('on');
-    });    
+    });
+    
+   // 02 visual
+   gsap .registerPlugin(ScrollTrigger);
+
+    gsap.timeline({
+        scrollTrigger: {
+            trigger: '.visual',
+            start: '100% 100%',
+            end: '100% 0',
+            scrub: 1,
+            markers: true
+        }
+    })
+    .to('.logoWrap #j', {x: -150, y: 250, rotate: 20, ease: 'none', duration: 5}, 0)  
+    .to('.logoWrap #y', {x: 30, y: 150, rotate: -10, ease: 'none', duration: 5}, 0)  
+    .to('.logoWrap #o', {x: 0, y: 400, rotate: -10, ease: 'none', duration: 5}, 0)  
+    .to('.logoWrap #u', {x: 50, y: 300, rotate: 10, ease: 'none', duration: 5}, 0)  
+    .to('.logoWrap #n', {x: 100, y: 100, rotate: -10, ease: 'none', duration: 5}, 0)  
+    .to('.logoWrap #g', {x: 50, y: 450, rotate: -20, ease: 'none', duration: 5}, 0)  
+    
 }
